2271ce2d9e265b5aa228965c1b8c07ce520f3957
malavikaac <acmalavika@gmail.com>
2023-11-14 16:54:25 +0530

    feat - created a account form page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'get_stared_page.dart';
+import 'screens/get_started_page.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -18,47 +19,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GetStared extends StatelessWidget {
-  const GetStared({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: CircleAvatar(
-        backgroundColor: const Color.fromARGB(255, 231, 243, 248),
-        child: Column(
-          children: [
-            const Spacer(),
-            const Text('BookTracker',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 31, 31, 31),
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold)),
-            const Text('"Read ..Change .. Yourself"',
-                style: TextStyle(
-                    color: Color.fromARGB(255, 107, 107, 107),
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400)),
-            const SizedBox(height: 50),
-            TextButton.icon(
-                style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 248, 250, 117)),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const LoginPage()));
-                },
-                icon: const Icon(
-                  Icons.login_rounded,
-                  color: Colors.black,
-                ),
-                label: const Text(
-                  'Sign in to get started',
-                  style: TextStyle(color: Colors.black),
-                )),
-            const Spacer(),
-          ],
-        ),
-      ),
-    );
-  }
-}
